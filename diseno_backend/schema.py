246d000ffdf37592985e9588cc0dfa819131f7d1
Alegxmx <zs21004536@estudiantes.uv.mx>
2023-10-03 04:32:19 -0600

    Agregando registro

--- diseno_backend/schema.py
+++ diseno_backend/schema.py
@@ -1,1 +1,28 @@
+import graphene
+from graphql_auth.schema import MeQuery
+from graphql_auth import mutations
+from graphql_auth.models import UserStatus
+
+class CustomRegister(mutations.Register):
+    class Input:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)  # Add email field
+
+    @classmethod
+    def perform_mutation(cls, root, info, username, email, **kwargs):
+        # Ensure that both username and email are unique
+        if cls.check_unique(username, email):
+            user = cls.create_user(username=username, email=email)
+            cls.create_user_status(user)
+            return cls(user=user)
+        else:
+            raise ValueError("Username and/or email already exist")
+
+class Query(MeQuery, graphene.ObjectType):
+    pass
+
+class Mutation(graphene.ObjectType):
+    register = CustomRegister.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
